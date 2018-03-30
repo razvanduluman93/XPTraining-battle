84a8d157e7cfa104e7322ad318633bc667634d42
Ionut Apetrei <ionut.apetrei@cegeka.com>
2018-03-30 13:51:59 +0200

    Adding Headquarter service.

---
+++ src/main/java/be/cegeka/battle/service/impl/HeadquartersServiceImpl.java
@@ -0,0 +1,21 @@
+package be.cegeka.battle.service.impl;
+
+import be.cegeka.battle.service.IHeadquarters;
+
+public class HeadquartersServiceImpl implements IHeadquarters {
+
+    @Override
+    public int reportEnlistment(String soldierName) {
+        return 0;
+    }
+
+    @Override
+    public void reportCasualty(int soldierId) {
+
+    }
+
+    @Override
+    public void reportVictory(int remainingNumberOfSoldiers) {
+
+    }
+}
